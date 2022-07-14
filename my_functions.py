21bf81e1499d01d6e49ab72f6d2bbc994902997a
Kiryl Pashkin <sleembelbox@gmail.com>
2022-07-14 12:54:08 +0300

    update functions

--- my_functions.py
+++ my_functions.py
@@ -4,7 +4,7 @@ def fill_list(items:int) -> list: # Get the filled array
     output_list = list()
     counter = 0
 
-    while counter >= items:
+    while counter < items:
         output_list.append(rnd.randint(1,50))
         counter += 1
     
