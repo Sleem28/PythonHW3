c67950616413037dc576d5384dbc09bb1f5ffb6e
Kiryl Pashkin <sleembelbox@gmail.com>
2022-07-14 12:30:43 +0300

    first commit

---
+++ MyFunctions.py
@@ -0,0 +1,12 @@
+import random as rnd
+#----------------------------------------------------------------------------------------------------------------------------------------+
+def fill_list(items:int) -> list: # Get the filled array
+    output_list = list()
+    counter = 0
+
+    while counter >= items:
+        output_list.append(rnd.randint(1,50))
+        counter += 1
+    
+    return output_list
+#----------------------------------------------------------------------------------------------------------------------------------------+
